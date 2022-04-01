369f25ffcd7355987ca86769d4af22174d87d03f
Milan Kotarac <kotaracmilan@hotmail.com>
2022-04-01 15:27:16 +0200

    init

---
+++ FilesZippingRestApi/src/main/java/com/votek/demo/zip/FilesZippingRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.votek.demo.zip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesZippingRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesZippingRestApiApplication.class, args);
+	}
+
+}
---
+++ FilesZippingRestApi/src/test/java/com/votek/demo/zip/FilesZippingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votek.demo.zip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesZippingRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
